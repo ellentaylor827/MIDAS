d2172ffbe9cfe99de00f652523ad16d614196039
Alexander Elwell <alexander.hayden.elwell@gmail.com>
2023-01-27 11:04:58 +0000

    removed .idea to stop conflicts across platforms

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+# import needed files
 import sys
 from PyQt6.QtWidgets import (
     QApplication,
 )
 from main_window import *
+# setting base directory for os
 basedir = os.path.dirname(__file__)
 
 
 if __name__ == '__main__':
-
     # creating the app window using main_window.py
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     app.exec()
+    
