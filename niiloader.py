f0cf549528c972fe4e3e58d8f3cb0450b93b4af1
Alexander Elwell <alexander.hayden.elwell@gmail.com>
2023-01-30 20:22:57 +0000

    added file import feature to MainWindow + updated niiloader.py

--- niiloader.py
+++ niiloader.py
@@ -2,21 +2,23 @@ import nibabel as nib
 import numpy as np
 import matplotlib.pyplot as plt
 
-#actual loading of nifty file
+
+# actual loading of nifty file
 def loadFile(filename):
     nifty = nib.load(filename).get_fdata()
     return nifty
 
 
-#placeholder function that plots and displays the file using pyplot
+# placeholder function that plots and displays the file using pyplot
 def showSlice(image, sliceNum):
-    slice = image[:, :, sliceNum] #selects the slice to be shown
-    plt.imshow(slice, cmap=colormap) #plots the slice onto a figure in grayscale
-    plt.show() #shows the figure
-
-colormap = 'gray'
-filename = "BraTS20_Training_001_t1.nii.gz"
-nifty = loadFile(filename)
-print("There are", nifty.shape[2], "slices to see.")
-sliceNum = int(input("which slice would you like to view? "))
-showSlice(nifty, sliceNum)
+    slice = image[:, :, sliceNum]  # selects the slice to be shown
+    plt.imshow(slice, cmap=colormap)  # plots the slice onto a figure in grayscale
+    plt.show()  # shows the figure
+#
+#
+# colormap = 'gray'
+# filename = "BraTS20_Training_001_t1.nii.gz"
+# nifty = loadFile(filename)
+# print("There are", nifty.shape[2], "slices to see.")
+# sliceNum = int(input("which slice would you like to view? "))
+# showSlice(nifty, sliceNum)
